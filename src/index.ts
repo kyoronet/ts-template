bd606e1ab055027ff486ba3ad15470c60f0c5309
kyoronet <42742556+kyoronet@users.noreply.github.com>
2019-10-14 02:21:40 +0900

    Lintワーニング修正

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,13 @@ import Koa from 'koa'
 
 const app = new Koa()
 
-async function start() {
-  const host: string = process.env.HOST || '127.0.0.1'
-  const port: number = Number(process.env.PORT) || 3000
+const host: string = process.env.HOST || '127.0.0.1'
+const port: number = Number(process.env.PORT) || 3000
 
-  app.use((ctx) => {
-    ctx.status = 200
-    ctx.respond = false // Bypass Koa's built-in response handling
-  })
+app.use((ctx) => {
+  ctx.status = 200
+  ctx.respond = false // Bypass Koa's built-in response handling
+})
 
-  app.listen(port, host)
-  console.log({
-    message: `Server listening on http://${host}:${port}`,
-    badge: true
-  })
-}
-
-start()
+app.listen(port, host)
+console.log(`Server listening on http://${host}:${port}`)
